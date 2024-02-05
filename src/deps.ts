ccb1148fb4ccc1a2ae2db784f3902845d57d0016
sgohlke <gohlke.stefan@yahoo.de>
2024-02-05 22:15:15 +0100

    Update dependencies and unstable flag

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,7 @@
-export { assert } from 'https://deno.land/std@0.212.0/assert/assert.ts'
-
 export {
+   assert,
    assertEquals,
-} from 'https://deno.land/std@0.212.0/assert/assert_equals.ts'
-
-export { fail } from 'https://deno.land/std@0.212.0/assert/fail.ts'
+   fail
+} from 'https://raw.githubusercontent.com/sgohlke/deno-deps/main/deps.ts'
 
 export * from 'https://raw.githubusercontent.com/sgohlke/rpg-ts-base/v0.3.0/src/index.ts'
