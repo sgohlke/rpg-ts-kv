c9b59c4b962c0d083deebd9990ce0f8bb78c6c16
sgohlke <gohlke.stefan@yahoo.de>
2024-05-06 23:35:56 +0200

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,1 @@
-export {
-   assert,
-   assertEquals,
-   fail
-} from 'https://raw.githubusercontent.com/sgohlke/deno-deps/main/deps.ts'
-
-export * from 'https://raw.githubusercontent.com/sgohlke/rpg-ts-base/v0.3.0/src/index.ts'
+export * from 'https://raw.githubusercontent.com/sgohlke/rpg-ts-base/v0.4.0/src/index.ts'
